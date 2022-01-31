f07a6cb587a8bda6b0b5377d6b9926c8e428e5d2
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-01-31 09:23:59 +0530

    WF-62291-RenameProjects

---
+++ Format rows and columns/Expand or Collapse Groups/NET Standard/Expand or Collapse Groups/Expand or Collapse Groups/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Expand_or_Collapse_Groups
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpandandCollapse obj = new ExpandandCollapse();
+
+            obj.ExpandGroups();
+            obj.CollapseGroups();
+        }
+    }
+    public class ExpandandCollapse
+    {
+        public void ExpandGroups()
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../InputTemplate - To Expand.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                #region Expand Groups
+                //Expand row groups
+                worksheet.Range["A3:A7"].ExpandGroup(ExcelGroupBy.ByRows, ExpandCollapseFlags.ExpandParent);
+                worksheet.Range["A11:A16"].ExpandGroup(ExcelGroupBy.ByRows);
+
+                //Expand column groups
+                worksheet.Range["C1:D1"].ExpandGroup(ExcelGroupBy.ByColumns, ExpandCollapseFlags.ExpandParent);
+                worksheet.Range["F1:G1"].ExpandGroup(ExcelGroupBy.ByColumns);
+                #endregion
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("ExpandGroups.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("ExpandGroups.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+        public void CollapseGroups()
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../InputTemplate - To Collapse.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                #region Collapse Groups
+                //Collapse row groups
+                worksheet.Range["A3:A7"].CollapseGroup(ExcelGroupBy.ByRows);
+                worksheet.Range["A11:A16"].CollapseGroup(ExcelGroupBy.ByRows);
+
+                //Collapse column groups
+                worksheet.Range["C1:D1"].CollapseGroup(ExcelGroupBy.ByColumns);
+                worksheet.Range["F1:G1"].CollapseGroup(ExcelGroupBy.ByColumns);
+                #endregion
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("CollapseGroups.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("CollapseGroups.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
---
+++ Format rows and columns/Ungroup Rows and Columns/NET Standard/Ungroup Rows and Columns/Ungroup Rows and Columns/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Ungroup_Rows_and_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../InputTemplate - ToUngroup.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                #region Un-Group Rows
+                //Ungroup Rows
+                worksheet.Range["A3:A7"].Ungroup(ExcelGroupBy.ByRows);
+                #endregion
+
+                #region Un-Group Columns
+                //Ungroup Columns
+                worksheet.Range["C1:D1"].Ungroup(ExcelGroupBy.ByColumns);
+                #endregion
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("UngroupRowsandColumns.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("UngroupRowsandColumns.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
