46c8c48819a54f7e771bd3130175f7219a5f6229
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-15 18:14:29 +0530

    Create Chart

---
+++ Create and Edit Charts/Create Chart/NET Standard/Create Chart/Create Chart/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Create_Chart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream, ExcelOpenType.Automatic);
+                IWorksheet sheet = workbook.Worksheets[0];
+
+                //Create a Chart
+                IChartShape chart = sheet.Charts.Add();
+
+                //Set Chart Type
+                chart.ChartType = ExcelChartType.Column_Clustered;
+
+                //Set data range in the worksheet
+                chart.DataRange = sheet.Range["A1:C6"];
+                chart.IsSeriesInRows = false;
+
+                //Set Datalabels
+                IChartSerie serie1 = chart.Series[0];
+                IChartSerie serie2 = chart.Series[1];
+
+                serie1.DataPoints.DefaultDataPoint.DataLabels.IsValue = true;
+                serie2.DataPoints.DefaultDataPoint.DataLabels.IsValue = true;
+                serie1.DataPoints.DefaultDataPoint.DataLabels.Position = ExcelDataLabelPosition.Outside;
+                serie2.DataPoints.DefaultDataPoint.DataLabels.Position = ExcelDataLabelPosition.Outside;
+
+                //Set Legend
+                chart.HasLegend = true;
+                chart.Legend.Position = ExcelLegendPosition.Bottom;
+
+                //Positioning the chart in the worksheet
+                chart.TopRow = 8;
+                chart.LeftColumn = 1;
+                chart.BottomRow = 23;
+                chart.RightColumn = 8;
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("Chart.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("Chart.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
