a5e2386310c62b0599e55119249d8740b9ec47a7
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-15 18:36:20 +0530

    Chart Sheet

---
+++ Create and Edit Charts/Chart Sheet/NET Standard/Chart Sheet/Chart Sheet/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Chart_Sheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream, ExcelOpenType.Automatic);
+                IWorksheet sheet = workbook.Worksheets[0];
+
+                //Add the chart sheet
+                IChart chart = workbook.Charts.Add();
+                chart.ChartType = ExcelChartType.Column_Clustered;
+                chart.DataRange = sheet.Range["A1:C6"];
+                chart.IsSeriesInRows = false;
+
+                //Set Legend
+                chart.HasLegend = true;
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("Chart.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("Chart.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
