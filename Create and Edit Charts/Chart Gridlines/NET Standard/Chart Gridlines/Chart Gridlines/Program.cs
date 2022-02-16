f7c7db1a6c2631fb3ad1ce8bf12f4fa7b074eec6
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-16 13:05:55 +0530

    Chart Gridlines

---
+++ Create and Edit Charts/Chart Gridlines/NET Standard/Chart Gridlines/Chart Gridlines/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Chart_Gridlines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Adding chart in the Excel worksheet
+                IChartShape chart = worksheet.Charts.Add();
+                chart.DataRange = worksheet.Range["A1:C5"];
+                chart.ChartType = ExcelChartType.Column_Clustered;
+                chart.IsSeriesInRows = false;
+
+                //Hiding major gridlines
+                chart.PrimaryValueAxis.HasMajorGridLines = false;
+
+                //Showing minor gridlines
+                chart.PrimaryValueAxis.HasMinorGridLines = true;
+
+                //Set Legend
+                chart.HasLegend = true;
+                chart.Legend.Position = ExcelLegendPosition.Bottom;
+
+                //Positioning the chart in the worksheet
+                chart.TopRow = 8;
+                chart.LeftColumn = 1;
+                chart.BottomRow = 23;
+                chart.RightColumn = 8;
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("Chart.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("Chart.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
