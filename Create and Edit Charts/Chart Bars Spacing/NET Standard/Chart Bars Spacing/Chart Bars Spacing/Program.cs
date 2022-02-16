b21f3d559636ba6204f9ebabd96034db98b25f73
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-16 12:39:35 +0530

    Chart Bars Spacing

---
+++ Create and Edit Charts/Chart Bars Spacing/NET Standard/Chart Bars Spacing/Chart Bars Spacing/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Chart_Bars_Spacing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Adding chart in the worksheet
+                IChartShape chart = worksheet.Charts.Add();
+                chart.DataRange = worksheet.Range["A1:C5"];
+                chart.ChartType = ExcelChartType.Column_Clustered;
+                chart.IsSeriesInRows = false;
+
+                //Adding space between bars of different series of single category
+                chart.Series[0].SerieFormat.CommonSerieOptions.Overlap = 60;
+
+                //Adding space between bars of different categories
+                chart.Series[0].SerieFormat.CommonSerieOptions.GapWidth = 80;
+
+                //Set Legend
+                chart.HasLegend = true;
+                chart.Legend.Position = ExcelLegendPosition.Bottom;
+
+                //Positioning the chart in the worksheet
+                chart.TopRow = 8;
+                chart.LeftColumn = 1;
+                chart.BottomRow = 23;
+                chart.RightColumn = 8;
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("Chart.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("Chart.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
