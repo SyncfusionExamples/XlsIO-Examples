941c790d234ba94b0bd71cf987665453d657d91c
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-16 12:29:43 +0530

    Chart Series Border

---
+++ Create and Edit Charts/Chart Series Border/NET Standard/Chart Series Border/Chart Series Border/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+using Syncfusion.Drawing;
+
+namespace Chart_Series_Border
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Adding chart in the worksheet
+                IChartShape chart = worksheet.Charts.Add();
+                chart.DataRange = worksheet.Range["A1:C5"];
+                chart.ChartType = ExcelChartType.Column_Clustered;
+                chart.IsSeriesInRows = false;
+
+                //Accessing first chart series
+                IChartSerie serie = chart.Series[0];
+
+                //Formatting the series border
+                serie.SerieFormat.LineProperties.LineColor = Color.Brown;
+                serie.SerieFormat.LineProperties.LinePattern = ExcelChartLinePattern.CircleDot;
+                serie.SerieFormat.LineProperties.LineWeight = ExcelChartLineWeight.Wide;
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("Chart.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("Chart.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
