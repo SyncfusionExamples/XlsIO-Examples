581261798887ebc2f4c33126b047ddc2015c5e22
sathiyaseelanksf3503 <Sathiyaseelan.kannigaraj@syncfusion.com>
2022-02-18 18:37:05 +0530

    Remove Column

---
+++ Create and Edit Table/Remove Column/NET Standard/Remove Column/Remove Column/Program.cs
@@ -0,0 +1,44 @@
+﻿using Syncfusion.XlsIO;
+using System;
+using System.IO;
+
+namespace Remove_Column
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Creating a table
+                IListObject table = worksheet.ListObjects.Create("Table1", worksheet["A1:C5"]);
+
+                // Removing a column from the table
+                worksheet.DeleteColumn(2, 1);
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("RemoveTable.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                inputStream.Dispose();
+                outputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("RemoveTable.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
