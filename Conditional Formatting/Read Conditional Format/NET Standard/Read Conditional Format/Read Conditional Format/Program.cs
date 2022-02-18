4f8e0d469a523c3e9a2baeaaad32e420561a71f7
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-18 12:15:33 +0530

    Read Conditional Format

---
+++ Conditional Formatting/Read Conditional Format/NET Standard/Read Conditional Format/Read Conditional Format/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Read_Conditional_Format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream, ExcelOpenType.Automatic);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Read conditional formatting settings 
+                string formatType = worksheet.Range["A1"].ConditionalFormats[0].FormatType.ToString();
+                string cfOperator = worksheet.Range["A1"].ConditionalFormats[0].Operator.ToString();
+
+                //Dispose streams
+                inputStream.Dispose();
+            }
+        }
+    }
+}
