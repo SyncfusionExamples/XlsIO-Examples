432b6c7cea41d1fd92647892d6a91d341cee43da
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-21 16:19:18 +0530

    Format Pivot Table

---
+++ Pivot Table/Format Pivot Table/NET Standard/Format Pivot Table/Format Pivot Table/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Format_Pivot_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[1];
+                IPivotTable pivotTable = worksheet.PivotTables[0];
+
+                //Set BuiltInStyle
+                pivotTable.BuiltInStyle = PivotBuiltInStyles.PivotStyleDark12;
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("PivotTable.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("PivotTable.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
