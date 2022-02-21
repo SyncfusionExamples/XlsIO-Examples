3fd66f681752528656dc69427b6a780b1a681aee
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-21 15:15:44 +0530

    Add SVG Picture

---
+++ Pictures in Excel/Add SVG Picture/NET Standard/Add SVG Picture/Add SVG Picture/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Add_SVG_Picture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                FileStream svgStream = new FileStream("../../../Data/Image.svg", FileMode.Open);
+                FileStream pngStream = new FileStream("../../../Data/Image.png", FileMode.Open);
+
+                //Add svg image with given svg and png streams
+                worksheet.Pictures.AddPicture(1, 1, svgStream, pngStream);
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("SVGImage.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                svgStream.Dispose();
+                pngStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("SVGImage.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
