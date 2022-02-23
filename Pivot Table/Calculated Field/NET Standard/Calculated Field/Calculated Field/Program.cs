ab57e9f00c494be8c59f42a9032127381aaef225
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-23 12:35:18 +0530

    Calculated Field

---
+++ Pivot Table/Calculated Field/NET Standard/Calculated Field/Calculated Field/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Calculated_Field
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet sheet = workbook.Worksheets[1];
+                IPivotTable pivotTable = sheet.PivotTables[0];
+
+                //Add calculated field to the first pivot table
+                IPivotField field = pivotTable.CalculatedFields.Add("Percent", "Units/3000*100");
+
+                //Set Field Formula
+                field.Formula = "Units/3000*200";
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("CalculatedField.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("CalculatedField.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
