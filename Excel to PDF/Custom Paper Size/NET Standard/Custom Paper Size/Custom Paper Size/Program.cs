03abffa767db3ca9b4bb813e8d4e79c9290b7359
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-24 19:34:13 +0530

    Custom Paper Size

---
+++ Excel to PDF/Custom Paper Size/NET Standard/Custom Paper Size/Custom Paper Size/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+using Syncfusion.Pdf;
+using Syncfusion.XlsIORenderer;
+using Syncfusion.Drawing;
+
+namespace Custom_Paper_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+
+                //Initialize XlsIORendererSettings
+                XlsIORendererSettings settings = new XlsIORendererSettings();
+
+                //Set CustomerPaperSize
+                settings.CustomPaperSize = new SizeF(10, 20);
+
+                //Initialize XlsIORenderer
+                XlsIORenderer renderer = new XlsIORenderer();
+
+                //Convert the Excel document to PDF with renderer settings
+                PdfDocument pdfDocument = renderer.ConvertToPDF(workbook, settings);
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("CustomPaperSize.pdf", FileMode.Create, FileAccess.Write);
+                pdfDocument.Save(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("CustomPaperSize.pdf")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
