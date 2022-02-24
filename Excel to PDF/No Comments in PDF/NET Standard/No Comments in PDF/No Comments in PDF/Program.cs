ce71f4af4fe2578d9b0d88b8629e4a15195fe56f
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-24 18:27:28 +0530

    No Comments in PDF

---
+++ Excel to PDF/No Comments in PDF/NET Standard/No Comments in PDF/No Comments in PDF/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+using Syncfusion.Pdf;
+using Syncfusion.XlsIORenderer;
+
+namespace No_Comments_in_PDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Set print location to comments
+                worksheet.PageSetup.PrintComments = ExcelPrintLocation.PrintNoComments;
+
+                //Initialize XlsIO renderer.
+                XlsIORenderer renderer = new XlsIORenderer();
+
+                //Convert Excel document into PDF document 
+                PdfDocument pdfDocument = renderer.ConvertToPDF(worksheet);
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("NoCommentsInPDF.pdf", FileMode.Create, FileAccess.Write);
+                pdfDocument.Save(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("NoCommentsInPDF.pdf")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
