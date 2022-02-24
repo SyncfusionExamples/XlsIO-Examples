edde2c6d41066d3055b95549b7dbb78115a8befc
sathiyaseelanksf3503 <Sathiyaseelan.kannigaraj@syncfusion.com>
2022-02-24 12:05:51 +0530

    Create Macro as Document

---
+++ Create and Edit Macros/Create Macro as Document/NET Standard/Create Macro as Document/Create Macro as Document/Program.cs
@@ -0,0 +1,49 @@
+﻿using Syncfusion.Office;
+using Syncfusion.XlsIO;
+using System;
+using System.IO;
+
+namespace Create_Macro_as_Document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet sheet = workbook.Worksheets[0];
+
+                //Creating Vba project
+                IVbaProject project = workbook.VbaProject;
+
+                //Accessing vba modules collection
+                IVbaModules vbaModules = project.Modules;
+
+                // Accessing sheet module
+                IVbaModule vbaModule = vbaModules[sheet.CodeName];
+
+                //Adding vba code to the module
+                vbaModule.Code = "Sub Auto_Open\n MsgBox \" Workbook Opened \" \n End Sub";
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("MacroAsDocument.xlsm", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream, ExcelSaveType.SaveAsMacro);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("MacroAsDocument.xlsm")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
