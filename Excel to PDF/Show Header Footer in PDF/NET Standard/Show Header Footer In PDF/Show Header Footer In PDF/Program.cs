63b4ec9de7c4d8c473bf2d56a47140a7ed984c6e
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-25 09:12:43 +0530

    Show Header Footer In PDF

---
+++ Excel to PDF/Show Header Footer in PDF/NET Standard/Show Header Footer In PDF/Show Header Footer In PDF/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+using Syncfusion.Pdf;
+using Syncfusion.XlsIORenderer;
+
+namespace Show_Header_Footer_In_PDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+
+                //Initialize XlsIORendererSettings
+                XlsIORendererSettings settings = new XlsIORendererSettings();
+
+                //Disable ShowHeader
+                settings.HeaderFooterOption.ShowHeader = false;
+
+                //Enable ShowFooter
+                settings.HeaderFooterOption.ShowFooter = true;
+
+                //Initialize XlsIORenderer
+                XlsIORenderer renderer = new XlsIORenderer();
+
+                //Convert the Excel document to PDF with renderer settings
+                PdfDocument pdfDocument = renderer.ConvertToPDF(workbook, settings);
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("HeaderFooterInPDF.pdf", FileMode.Create, FileAccess.Write);
+                pdfDocument.Save(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("HeaderFooterInPDF.pdf")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
