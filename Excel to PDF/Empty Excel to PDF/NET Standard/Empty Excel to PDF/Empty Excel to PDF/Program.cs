db08a69efb3defc1f21164d0bd590ca9d873b55f
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-25 12:35:34 +0530

    Empty Excel to PDF

---
+++ Excel to PDF/Empty Excel to PDF/NET Standard/Empty Excel to PDF/Empty Excel to PDF/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+using Syncfusion.Pdf;
+using Syncfusion.XlsIORenderer;
+
+namespace Empty_Excel_to_PDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                IWorkbook workbook = application.Workbooks.Create(1);
+
+                //Initialize XlsIORendererSettings
+                XlsIORendererSettings settings = new XlsIORendererSettings();
+
+                //Enabling ThrowWhenExcelFileIsEmpty throws exception
+                settings.ThrowWhenExcelFileIsEmpty = true;
+
+                //Initialize XlsIORenderer
+                XlsIORenderer renderer = new XlsIORenderer();
+
+                //Convert the Excel document to PDF with renderer settings
+                PdfDocument pdfDocument = renderer.ConvertToPDF(workbook, settings);
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("EmptyExcelToPDF.pdf", FileMode.Create, FileAccess.Write);
+                pdfDocument.Save(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("EmptyExcelToPDF.pdf")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
