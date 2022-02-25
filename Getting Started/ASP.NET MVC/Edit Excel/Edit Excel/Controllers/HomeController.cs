ae8a4c6a812708c12a3282fa330dd1914ef96a9d
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-25 18:17:39 +0530

    Edit Excel in ASP.NET MVC

---
+++ Getting Started/ASP.NET MVC/Edit Excel/Edit Excel/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Syncfusion.XlsIO;
+
+namespace Edit_Excel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EditDocument()
+        {
+            //Create an instance of ExcelEngine
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                //Instantiate the Excel application object
+                IApplication application = excelEngine.Excel;
+
+                //Set the default application version
+                application.DefaultVersion = ExcelVersion.Xlsx;
+
+                //Load the existing Excel workbook into IWorkbook
+                IWorkbook workbook = application.Workbooks.Open(Server.MapPath("App_Data/InputTemplate.xlsx"));
+
+                //Get the first worksheet in the workbook into IWorksheet
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Assign some text in a cell
+                worksheet.Range["A3"].Text = "Hello World";
+
+                //Save the Excel document
+                workbook.SaveAs("EditExcel.xlsx", HttpContext.ApplicationInstance.Response, ExcelDownloadType.Open);
+            }
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Getting Started/ASP.NET MVC/Edit Excel/Edit Excel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Edit_Excel.MvcApplication" Language="C#" %>
