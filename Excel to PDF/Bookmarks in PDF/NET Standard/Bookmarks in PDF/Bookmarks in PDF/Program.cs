f7472ba2919314f5d1b30f73af3ae04256a1ca51
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-25 08:25:03 +0530

    Bookmarks in PDF

---
+++ Excel to PDF/Bookmarks in PDF/NET Standard/Bookmarks in PDF/Bookmarks in PDF/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+using Syncfusion.XlsIORenderer;
+using Syncfusion.Pdf;
+
+namespace Bookmarks_in_PDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+
+                //Initialize XlsIORendererSettings
+                XlsIORendererSettings settings = new XlsIORendererSettings();
+
+                //Disable ExportBookmarks
+                settings.ExportBookmarks = false;
+
+                //Initialize XlsIORenderer
+                XlsIORenderer renderer = new XlsIORenderer();
+
+                //Convert the Excel document to PDF with renderer settings
+                PdfDocument pdfDocument = renderer.ConvertToPDF(workbook, settings);
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("BookmarksInPDF.pdf", FileMode.Create, FileAccess.Write);
+                pdfDocument.Save(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("BookmarksInPDF.pdf")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
