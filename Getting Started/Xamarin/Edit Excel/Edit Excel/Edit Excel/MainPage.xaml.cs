8960944bd34099098dc2500df423ad81292412a2
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-25 22:14:27 +0530

    Edit Excel in Xamarin

---
+++ Getting Started/Xamarin/Edit Excel/Edit Excel/Edit Excel/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Syncfusion.XlsIO;
+using System.Reflection;
+using System.IO;
+
+namespace Edit_Excel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+		void OnButtonClicked(object sender, EventArgs e)
+		{
+			using (ExcelEngine excelEngine = new ExcelEngine())
+			{
+				IApplication application = excelEngine.Excel;
+				application.DefaultVersion = ExcelVersion.Xlsx;
+
+				//"App" is the class of Portable project.
+				Assembly assembly = typeof(App).GetTypeInfo().Assembly;
+				Stream fileStream = assembly.GetManifestResourceStream("Edit_Excel.InputTemplate.xlsx");
+
+				//Opens the workbook 
+				IWorkbook workbook = application.Workbooks.Open(fileStream);
+
+				//Access first worksheet from the workbook.
+				IWorksheet worksheet = workbook.Worksheets[0];
+
+				//Set Text in cell A3.
+				worksheet.Range["A3"].Text = "Hello World";
+
+				MemoryStream stream = new MemoryStream();
+				workbook.SaveAs(stream);
+
+				workbook.Close();
+
+				//Save the stream into XLSX file
+				Xamarin.Forms.DependencyService.Get<ISave>().SaveAndView("EditExcel.xlsx", "application/msexcel", stream);
+			}
+		}
+    }
+}
