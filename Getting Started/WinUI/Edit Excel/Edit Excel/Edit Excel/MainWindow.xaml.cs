8dfe25518d7b15670eb72c63f60b6af4855703fc
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-02-28 08:50:38 +0530

    Edit Excel in WinUI

---
+++ Getting Started/WinUI/Edit Excel/Edit Excel/Edit Excel/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Syncfusion.XlsIO;
+using System.Reflection;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Edit_Excel
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void EditExcel(object sender, RoutedEventArgs e)
+        {
+            //Create an instance of ExcelEngine.
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+
+                Assembly executingAssembly = typeof(App).GetTypeInfo().Assembly;
+                Stream inputStream = executingAssembly.GetManifestResourceStream("Edit_Excel.InputTemplate.xlsx");
+
+                //Create a workbook with a worksheet
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+
+                //Access first worksheet from the workbook instance.
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Set Text in cell A3.
+                worksheet.Range["A3"].Text = "Hello World";
+
+                MemoryStream stream = new MemoryStream();
+                workbook.SaveAs(stream);
+                Save(stream, "EditExcel");
+            }
+        }
+        async void Save(MemoryStream stream, string filename)
+        {
+
+            StorageFile stFile;
+            if (!(Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons")))
+            {
+                FileSavePicker savePicker = new FileSavePicker();
+                savePicker.DefaultFileExtension = ".xlsx";
+                savePicker.SuggestedFileName = filename;
+                savePicker.FileTypeChoices.Add("Excel Documents", new List<string>() { ".xlsx" });
+                var hwnd = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+                WinRT.Interop.InitializeWithWindow.Initialize(savePicker, hwnd);
+                stFile = await savePicker.PickSaveFileAsync();
+            }
+            else
+            {
+                StorageFolder local = Windows.Storage.ApplicationData.Current.LocalFolder;
+                stFile = await local.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+            }
+            if (stFile != null)
+            {
+                using (IRandomAccessStream zipStream = await stFile.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    //Write compressed data from memory to file
+                    using (Stream outstream = zipStream.AsStreamForWrite())
+                    {
+                        byte[] buffer = stream.ToArray();
+                        outstream.Write(buffer, 0, buffer.Length);
+                        outstream.Flush();
+                    }
+                }
+            }
+            //Launch the saved Excel file
+            await Windows.System.Launcher.LaunchFileAsync(stFile);
+        }
+    }
+}
