fb55db0f1f632c668c3baa07c1ed8e5bf76cd30f
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2023-04-05 17:46:24 +0530

    Rotation and transparency to background image

---
+++ Pictures in Excel/Rotation and Transparency/NET Standard/Rotation and Transparency/Rotation and Transparency/Program.cs
@@ -0,0 +1,51 @@
+﻿using Syncfusion.XlsIO;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Rotation_and_Transparency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+
+                //Load an existing Excel file into IWorkbook
+                FileStream inputStream = new FileStream("../../../Data/Sample.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                Image image = Image.FromFile("../../../Data/image.png");
+                Bitmap bitmap = new Bitmap(image);
+                bitmap.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                bitmap.MakeTransparent(Color.Black);
+                bitmap.Save("image_M.png", ImageFormat.Png);
+
+                FileStream imageStream = new FileStream("image_M.png", FileMode.Open, FileAccess.Read);
+                worksheet.PageSetup.BackgoundImage = Syncfusion.Drawing.Image.FromStream(imageStream);
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("Output.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("Output.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
