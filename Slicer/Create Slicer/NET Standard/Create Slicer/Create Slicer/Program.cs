4053e0058b8525738a6e503e5f8c03fe620a6d6f
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2023-04-10 13:31:05 +0530

    Slicer

---
+++ Slicer/Create Slicer/NET Standard/Create Slicer/Create Slicer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Create_Slicer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream, ExcelOpenType.Automatic);
+                IWorksheet sheet = workbook.Worksheets[0];
+
+                //Access the table.
+                IListObject table = sheet.ListObjects[0];
+
+                //Add slicer for the table.
+                sheet.Slicers.Add(table, 3, 11, 2);
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("CreateSlicer.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("CreateSlicer.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
