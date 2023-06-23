bc26ee7188e0f61efa4f68f5dc3fabd582569909
MOHAN CHANDRAN <93247949+Mohan2401@users.noreply.github.com>
2023-06-23 09:29:39 +0530

    Excel to Thumbnail image conversion sample

---
+++ Real Time Examples in Excel/Excel To Thumbnail Image/Excel To Thumbnail Image/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Syncfusion.XlsIO;
+using Syncfusion.XlsIORenderer;
+
+namespace Excel_To_Thumbnail_Image
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("../../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet sheet = workbook.Worksheets[0];
+
+                //Initialize XlsIORenderer
+                application.XlsIORenderer = new XlsIORenderer();
+
+                //Convert to image
+                MemoryStream outputStream = new MemoryStream();
+                sheet.ConvertToImage(sheet.UsedRange, outputStream);
+
+                //Resize image to thumbnail size
+                System.Drawing.Image image = System.Drawing.Image.FromStream(outputStream);
+                System.Drawing.Image thumbnail = image.GetThumbnailImage(100, 100, () => false, IntPtr.Zero);
+
+                //Save image
+                thumbnail.Save("Image.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            }
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo = new System.Diagnostics.ProcessStartInfo("Image.png")
+            {
+                UseShellExecute = true
+            };
+            process.Start();
+        }
+    }
+}
