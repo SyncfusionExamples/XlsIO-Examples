52b5033e5f24e516ec1d1e89b558c0da8773ebf0
MOHAN CHANDRAN <93247949+Mohan2401@users.noreply.github.com>
2023-07-06 17:27:53 +0530

    Income tax email distribution sample

---
+++ Real Time Examples in Excel/IncomeTaxEmailDistribution/IncomeTaxEmailDistribution/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace IncomeTaxEmailDistribution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EmailSendButton = new System.Windows.Forms.Button();
+            this.excelPathTxtBox = new System.Windows.Forms.TextBox();
+            this.partAPathTxtBox = new System.Windows.Forms.TextBox();
+            this.partBPathTxtBox = new System.Windows.Forms.TextBox();
+            this.ExcelPathLabel = new System.Windows.Forms.Label();
+            this.PartAPathLabel = new System.Windows.Forms.Label();
+            this.PartBPathLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panColumnIdTxtBox = new System.Windows.Forms.TextBox();
+            this.emailColumnIdTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // EmailSendButton
+            // 
+            this.EmailSendButton.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailSendButton.Location = new System.Drawing.Point(309, 332);
+            this.EmailSendButton.Name = "EmailSendButton";
+            this.EmailSendButton.Size = new System.Drawing.Size(140, 42);
+            this.EmailSendButton.TabIndex = 0;
+            this.EmailSendButton.Text = "Send Mails";
+            this.EmailSendButton.UseVisualStyleBackColor = true;
+            this.EmailSendButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // excelPathTxtBox
+            // 
+            this.excelPathTxtBox.Location = new System.Drawing.Point(240, 162);
+            this.excelPathTxtBox.Name = "excelPathTxtBox";
+            this.excelPathTxtBox.Size = new System.Drawing.Size(403, 22);
+            this.excelPathTxtBox.TabIndex = 1;
+            // 
+            // partAPathTxtBox
+            // 
+            this.partAPathTxtBox.Location = new System.Drawing.Point(240, 210);
+            this.partAPathTxtBox.Name = "partAPathTxtBox";
+            this.partAPathTxtBox.Size = new System.Drawing.Size(403, 22);
+            this.partAPathTxtBox.TabIndex = 2;
+            // 
+            // partBPathTxtBox
+            // 
+            this.partBPathTxtBox.Location = new System.Drawing.Point(240, 255);
+            this.partBPathTxtBox.Name = "partBPathTxtBox";
+            this.partBPathTxtBox.Size = new System.Drawing.Size(403, 22);
+            this.partBPathTxtBox.TabIndex = 3;
+            // 
+            // ExcelPathLabel
+            // 
+            this.ExcelPathLabel.AutoSize = true;
+            this.ExcelPathLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExcelPathLabel.Location = new System.Drawing.Point(90, 162);
+            this.ExcelPathLabel.Name = "ExcelPathLabel";
+            this.ExcelPathLabel.Size = new System.Drawing.Size(111, 21);
+            this.ExcelPathLabel.TabIndex = 4;
+            this.ExcelPathLabel.Text = "Excel File Path";
+            // 
+            // PartAPathLabel
+            // 
+            this.PartAPathLabel.AutoSize = true;
+            this.PartAPathLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PartAPathLabel.Location = new System.Drawing.Point(89, 210);
+            this.PartAPathLabel.Name = "PartAPathLabel";
+            this.PartAPathLabel.Size = new System.Drawing.Size(118, 21);
+            this.PartAPathLabel.TabIndex = 5;
+            this.PartAPathLabel.Text = "Part A File Path";
+            // 
+            // PartBPathLabel
+            // 
+            this.PartBPathLabel.AutoSize = true;
+            this.PartBPathLabel.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PartBPathLabel.Location = new System.Drawing.Point(89, 261);
+            this.PartBPathLabel.Name = "PartBPathLabel";
+            this.PartBPathLabel.Size = new System.Drawing.Size(118, 21);
+            this.PartBPathLabel.TabIndex = 6;
+            this.PartBPathLabel.Text = "Part B File Path";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Email Column Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(89, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "PAN Column Id";
+            // 
+            // panColumnIdTxtBox
+            // 
+            this.panColumnIdTxtBox.Location = new System.Drawing.Point(240, 62);
+            this.panColumnIdTxtBox.Name = "panColumnIdTxtBox";
+            this.panColumnIdTxtBox.Size = new System.Drawing.Size(403, 22);
+            this.panColumnIdTxtBox.TabIndex = 9;
+            // 
+            // emailColumnIdTxtBox
+            // 
+            this.emailColumnIdTxtBox.Location = new System.Drawing.Point(240, 115);
+            this.emailColumnIdTxtBox.Name = "emailColumnIdTxtBox";
+            this.emailColumnIdTxtBox.Size = new System.Drawing.Size(403, 22);
+            this.emailColumnIdTxtBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 408);
+            this.Controls.Add(this.emailColumnIdTxtBox);
+            this.Controls.Add(this.panColumnIdTxtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PartBPathLabel);
+            this.Controls.Add(this.PartAPathLabel);
+            this.Controls.Add(this.ExcelPathLabel);
+            this.Controls.Add(this.partBPathTxtBox);
+            this.Controls.Add(this.partAPathTxtBox);
+            this.Controls.Add(this.excelPathTxtBox);
+            this.Controls.Add(this.EmailSendButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EmailSendButton;
+        private System.Windows.Forms.TextBox excelPathTxtBox;
+        private System.Windows.Forms.TextBox partAPathTxtBox;
+        private System.Windows.Forms.TextBox partBPathTxtBox;
+        private System.Windows.Forms.Label ExcelPathLabel;
+        private System.Windows.Forms.Label PartAPathLabel;
+        private System.Windows.Forms.Label PartBPathLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox panColumnIdTxtBox;
+        private System.Windows.Forms.TextBox emailColumnIdTxtBox;
+    }
+}
+
---
+++ Real Time Examples in Excel/IncomeTaxEmailDistribution/IncomeTaxEmailDistribution/Form1.cs
@@ -0,0 +1,155 @@
+﻿using Syncfusion.XlsIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IncomeTaxEmailDistribution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string panColumnId = panColumnIdTxtBox.Text.Trim('"');
+
+            string emailColumnId = emailColumnIdTxtBox.Text.Trim('"');
+
+            string excelFilePath = excelPathTxtBox.Text.Trim('"');
+
+            string[] partAPathFiles = Directory.GetFiles(partAPathTxtBox.Text.Trim('"'));
+
+            string[] partBPathFiles = Directory.GetFiles(partBPathTxtBox.Text.Trim('"'));
+
+            if(partAPathFiles != null && partAPathFiles.Length == 0)
+            {
+                MessageBox.Show("No files found in Part A Path");
+                return;
+            }
+            if(partBPathFiles != null && partBPathFiles.Length == 0)
+            {
+                MessageBox.Show("No files found in Part B Path");
+                return;
+            }
+            if(!File.Exists(excelFilePath))
+            {
+                MessageBox.Show("Excel File not found");
+                return;
+            }
+            
+            using(ExcelEngine excelengine = new ExcelEngine())
+            {
+                IApplication application = excelengine.Excel;
+                IWorkbook workbook = application.Workbooks.Open(excelFilePath);
+
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                IRange used = worksheet.UsedRange;
+
+                int startRow = used.Row + 1;
+                int endRow = used.LastRow;
+
+                for(int row = startRow;row <= endRow;row++)
+                {
+                    if (worksheet.Range[row, 10].Value == "Sent")
+                    {
+                        continue;
+                    }
+
+                    //Converting the Column Id to Column Index
+                    IRange panRange = worksheet[panColumnId + row.ToString()];
+
+                    string panID = worksheet.Range[row, panRange.Column].Value;
+
+                    if(string.IsNullOrEmpty(panID))
+                    {
+                        worksheet[row, 10].Value = "PAN not found";
+                        continue;
+                    }
+                    
+                    IRange emailRange = worksheet[emailColumnId + row.ToString()];
+
+                    string emailId = worksheet.Range[row, emailRange.Column].Value;
+
+                    string partAPath = partAPathFiles.Where(x => x.Contains(panID + "_")).FirstOrDefault();
+
+                    if (partAPath == null)
+                    {
+                        worksheet[row, 10].Value = "Part A File not found";
+                        continue;
+                    }
+                    
+                    string partBPath = partBPathFiles.Where(x => x.Contains(panID + "_")).FirstOrDefault();
+
+                    if (partBPath == null)
+                    {
+                        worksheet[row, 10].Value = "Part B File not found";
+                        continue;
+                    }
+                    try
+                    {
+                        //Your maid id
+                        string from = "Yourmail@abc.com";
+
+                        string subject = "Form 16 for the financial year 2022- 2023";
+
+                        string mailBody = "<p>Hi,</p>\r\n<p>Thank you for your kind cooperation</p>\r\n<p>We have attached the form 16 document pertaining to the financial year (2022-2023) tax deduction. Employees are requested to review and file their Income tax return before due date.</p>\r\n<p>The last date of filing of Income Tax return is July 31, 2023.</p>\r\n<p>For further assistance, please feel free to get back to us.</p>\r\n<p>Thanks and Regards,</p>\r\n<p>Accounts Team.</p>";
+
+                        SendEMail(from, emailId, subject, mailBody, partAPath, partBPath);
+
+                        worksheet[row, 10].Value = "Sent";
+                    }
+                    catch(Exception ex){
+                        worksheet[row, 10].Value = ex.ToString();
+                    }
+                }
+
+                Thread.Sleep(1000);
+
+                string filePath = Path.GetFileNameWithoutExtension(excelFilePath) + "_Updated_" + DateTime.Now.ToString().Replace(":", "-") + Path.GetExtension(excelFilePath); 
+               
+                workbook.SaveAs(filePath);
+                
+            }
+            Close();
+            
+        }
+        private static void SendEMail(string from, string recipients, string subject, string body, string attachmentPartA, string attachmentPartB)
+        {
+            //Creates the email message
+            var emailMessage = new MailMessage(from, recipients);
+            //Adds the subject for email
+            emailMessage.Subject = subject;
+            //Sets the HTML string as email body
+            emailMessage.IsBodyHtml = true;
+            emailMessage.Body = body;
+            emailMessage.Attachments.Add(new Attachment(attachmentPartA));
+            emailMessage.Attachments.Add(new Attachment(attachmentPartB));
+
+            //Sends the email with prepared message
+            using (var client = new SmtpClient())
+            {
+                //Update your SMTP Server address here
+                client.Host = "outlook.office365.com";
+                client.UseDefaultCredentials = false;
+                //Update your email credentials here. Need to generate app password for your mail id
+                client.Credentials = new System.Net.NetworkCredential(from, "apppassword");
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.Send(emailMessage);                
+            }
+        }
+    }
+}
