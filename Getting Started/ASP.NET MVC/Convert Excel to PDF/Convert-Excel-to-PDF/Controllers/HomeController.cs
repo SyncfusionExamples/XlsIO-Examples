cf13306a85d81ece462d78c2f9b35712c637f8b9
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2023-08-01 18:58:45 +0530

    Adding the Conversion of Excel to PDF in ASP.NET MVC

---
+++ Getting Started/ASP.NET MVC/Convert Excel to PDF/Convert-Excel-to-PDF/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Syncfusion.ExcelToPdfConverter;
+using Syncfusion.Pdf;
+using Syncfusion.XlsIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Convert_Excel_to_PDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult ConvertExcelToPdf()
+        {
+            //Create an instance of ExcelEngine
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+
+                //Load an existing file
+                FileStream excelStream = new FileStream(Server.MapPath("~/App_Data/InputTemplate.xlsx"), FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(excelStream);
+
+                //Initialize ExcelToPdfConverter
+                ExcelToPdfConverter converter = new ExcelToPdfConverter(workbook);
+
+                //Initialize PDF document
+                PdfDocument pdfDocument = new PdfDocument();
+
+                //Convert Excel document into PDF document
+                pdfDocument = converter.Convert();
+
+                //Create the MemoryStream to save the converted PDF.      
+                MemoryStream pdfStream = new MemoryStream();
+
+                //Save the converted PDF document to MemoryStream.
+                pdfDocument.Save(pdfStream);
+                pdfStream.Position = 0;
+
+                //Download PDF document in the browser.
+                return File(pdfStream, "application/pdf", "Sample.pdf");
+            }
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
