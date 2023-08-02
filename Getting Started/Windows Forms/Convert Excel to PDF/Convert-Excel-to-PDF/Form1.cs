74f6f7e370fe784c6ab1e599dd97dc01c4f7d639
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2023-08-02 16:07:39 +0530

    Adding the Conversion of Excel to PDF in WindowForms

---
+++ Getting Started/Windows Forms/Convert Excel to PDF/Convert-Excel-to-PDF/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Syncfusion.ExcelToPdfConverter;
+using Syncfusion.Pdf;
+using Syncfusion.XlsIO;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Convert_Excel_to_PDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //Create an instance of ExcelEngine
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+
+                //Load an existing file
+                FileStream excelStream = new FileStream("../../Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(excelStream);
+
+                //Initialize ExcelToPdfConverter
+                ExcelToPdfConverter converter = new ExcelToPdfConverter(workbook);
+
+                //Initialize PDF document
+                PdfDocument pdfDocument = new PdfDocument();
+
+                //Convert Excel document into PDF document
+                pdfDocument = converter.Convert();
+
+                //Save the converted PDF document to MemoryStream.
+                pdfDocument.Save("Sample.pdf");
+
+            }
+            //Launch the PDF file
+            System.Diagnostics.Process.Start("Sample.pdf");
+        }
+    }
+}
