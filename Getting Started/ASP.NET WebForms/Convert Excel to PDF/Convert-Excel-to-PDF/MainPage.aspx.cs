2809ecc62f3f823a096431ef1be47a00ada25517
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2023-08-02 12:28:20 +0530

    Adding the Conversion of Excel to PDF in ASP.NET WebForms

---
+++ Getting Started/ASP.NET WebForms/Convert Excel to PDF/Convert-Excel-to-PDF/MainPage.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Syncfusion.ExcelToPdfConverter;
+using Syncfusion.Pdf;
+using Syncfusion.XlsIO;
+using System.Web;
+
+namespace Convert_Excel_to_PDF
+{
+    public partial class MainPage : System.Web.UI.Page
+    {
+        protected void OnButtonClicked(object sender, EventArgs e)
+        {
+            //Create an instance of ExcelEngine
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+
+                //Load an existing file
+                FileStream excelStream = new FileStream(Server.MapPath("~/App_Data/InputTemplate.xlsx"), FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(excelStream);
+
+                //Initialize ExcelToPdfConverter
+                ExcelToPdfConverter converter = new ExcelToPdfConverter(workbook);
+
+                //Initialize PDF document
+                PdfDocument pdfDocument = new PdfDocument();
+
+                //Convert Excel document into PDF document
+                pdfDocument = converter.Convert();
+
+                //Create the MemoryStream to save the converted PDF.      
+                MemoryStream pdfStream = new MemoryStream();
+
+                //Save the converted PDF document to MemoryStream.
+                pdfDocument.Save("sample.pdf", HttpContext.Current.Response, HttpReadType.Save);
+            }
+        }
+    }
+}
