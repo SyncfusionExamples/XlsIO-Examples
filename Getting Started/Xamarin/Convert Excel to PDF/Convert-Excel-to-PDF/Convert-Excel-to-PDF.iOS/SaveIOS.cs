0180ee03a9dd011b4cd85c75393eb98ecf796ab0
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2023-08-04 12:10:51 +0530

    Add the conversion of Excel to PDF in Xamarin

---
+++ Getting Started/Xamarin/Convert Excel to PDF/Convert-Excel-to-PDF/Convert-Excel-to-PDF.iOS/SaveIOS.cs
@@ -0,0 +1,42 @@
+﻿using Foundation;
+using QuickLook;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using UIKit;
+
+[assembly: Dependency(typeof(SaveIOS))]
+
+class SaveIOS : ISave
+{
+    //Method to save document as a file and view the saved document
+    public async Task SaveAndView(string filename, string contentType, MemoryStream stream)
+    {
+        //Get the root path in iOS device.
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        string filePath = Path.Combine(path, filename);
+
+        //Create a file and write the stream into it.
+        FileStream fileStream = File.Open(filePath, FileMode.Create);
+        stream.Position = 0;
+        stream.CopyTo(fileStream);
+        fileStream.Flush();
+        fileStream.Close();
+
+        //Invoke the saved document for viewing
+        UIViewController currentController = UIApplication.SharedApplication.KeyWindow.RootViewController;
+        while (currentController.PresentedViewController != null)
+            currentController = currentController.PresentedViewController;
+        UIView currentView = currentController.View;
+
+        QLPreviewController qlPreview = new QLPreviewController();
+        QLPreviewItem item = new QLPreviewItemBundle(filename, filePath);
+        qlPreview.DataSource = new PreviewControllerDS(item);
+
+        currentController.PresentViewController(qlPreview, true, null);
+    }
+}
