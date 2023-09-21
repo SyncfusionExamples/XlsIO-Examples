2fb6b5c21a5c534ac7aabd672b55ba6cbeb80228
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2023-09-21 19:57:24 +0530

    TemplateMarker Formulas

---
+++ Import Data to Template/TemplateMarker-with-Formulas/NET Standard/TemplateMarker-with-Formulas/TemplateMarker-with-Formulas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TemplateMarker_with_Formulas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
