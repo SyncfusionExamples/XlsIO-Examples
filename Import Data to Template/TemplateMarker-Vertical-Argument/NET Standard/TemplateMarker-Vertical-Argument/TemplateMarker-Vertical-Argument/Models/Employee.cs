f5b80e0290a17d33018a4e42bf25ec66fbca8a94
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2023-09-21 19:55:39 +0530

    TemplateMarker Vertical Argument

---
+++ Import Data to Template/TemplateMarker-Vertical-Argument/NET Standard/TemplateMarker-Vertical-Argument/TemplateMarker-Vertical-Argument/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿namespace TemplateMarker_Vertical_Argument.Models
+{
+    public class Employee
+    {
+        private string m_name;
+        private int m_id;
+        private int m_age;
+
+        public string Name
+        {
+            get
+            {
+                return m_name;
+            }
+
+            set
+            {
+                m_name = value;
+            }
+        }
+        public int Id
+        {
+            get
+            {
+                return m_id;
+            }
+
+            set
+            {
+                m_id = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return m_age;
+            }
+
+            set
+            {
+                m_age = value;
+            }
+        }
+    }
+}
