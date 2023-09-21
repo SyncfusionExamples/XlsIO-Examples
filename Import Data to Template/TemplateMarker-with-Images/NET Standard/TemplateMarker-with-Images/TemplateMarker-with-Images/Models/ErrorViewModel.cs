07325d6d3d82bf2413d172f5b02f31bf09c12f8d
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2023-09-21 19:58:04 +0530

    TemplateMarker Images

---
+++ Import Data to Template/TemplateMarker-with-Images/NET Standard/TemplateMarker-with-Images/TemplateMarker-with-Images/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TemplateMarker_with_Images.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
