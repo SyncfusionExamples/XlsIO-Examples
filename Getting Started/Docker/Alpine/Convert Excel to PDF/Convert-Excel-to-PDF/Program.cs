ef667ffbeb58263cbb4c42f82ce73f57dab91a0f
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2023-10-11 16:39:29 +0530

    Excel to PDF in Docker

---
+++ Getting Started/Docker/Alpine/Convert Excel to PDF/Convert-Excel-to-PDF/Program.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.XlsIO;
+using System;
+using System.IO;
+using Syncfusion.XlsIORenderer;
+
+namespace Convert_Excel_to_PDF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream excelStream = new FileStream("Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(excelStream);
+
+                //Initialize XlsIO renderer.
+                XlsIORenderer renderer = new XlsIORenderer();
+
+                //Convert Excel document into PDF document 
+                PdfDocument pdfDocument = renderer.ConvertToPDF(workbook);
+
+                //Create the FileStream to save the converted PDF.
+                FileStream pdfStream = new FileStream("Output.pdf", FileMode.Create, FileAccess.ReadWrite);
+                pdfDocument.Save(pdfStream);
+            }
+        }
+    }
+}
