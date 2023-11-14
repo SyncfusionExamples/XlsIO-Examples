5fac3942732342a7e48f6a119a4b9b1d70da89a1
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2023-11-14 09:35:30 +0530

    Remove Create Excel

--- Getting Started/GCP/Create Excel/Create Excel/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Create_Excel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
