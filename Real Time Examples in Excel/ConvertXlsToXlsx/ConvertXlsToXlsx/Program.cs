acb97d7dac66e4f1ec4508242c48430e1f0cf40f
MOHAN CHANDRAN <93247949+Mohan2401@users.noreply.github.com>
2024-01-15 22:06:49 +0530

    C# Example for converting XLS to XLSX format

---
+++ Real Time Examples in Excel/ConvertXlsToXlsx/ConvertXlsToXlsx/Program.cs
@@ -0,0 +1,42 @@
+﻿using Syncfusion.XlsIO;
+
+
+namespace ConvertXlsToXlsx
+{
+    class Program
+    {
+        private static string inputPath = @"../../../Data/";
+
+        private static string outputPath = @"../../../Output/";
+        static void Main(string[] args)
+        {
+            string fileName = "Report.xls";
+
+            //Split the Excel document
+            ConvertXlsToXLSX(inputPath + fileName);
+        }
+        /// <summary>
+        /// Convert the Excel document from the given path
+        /// </summary>
+        /// <param name="filePath">Excel file path</param>
+        private static void ConvertXlsToXLSX(string filePath)
+        {
+            FileStream inputData = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite);
+
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Open(inputData);
+                IWorksheets worksheets = workbook.Worksheets;
+
+                workbook.Version = ExcelVersion.Xlsx;
+
+                FileStream fileStream = new FileStream(outputPath + Path.GetFileNameWithoutExtension(filePath) + ".xlsx", FileMode.Create, FileAccess.ReadWrite);
+                workbook.SaveAs(fileStream);
+                fileStream.Close();
+            }
+        }
+    }
+}
+
