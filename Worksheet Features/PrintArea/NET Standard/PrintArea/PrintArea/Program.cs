48b1897a243c123ac9cc6b64d0d7edc4889fd045
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2024-02-11 22:18:28 +0530

    PageSetup Settings

---
+++ Worksheet Features/PrintArea/NET Standard/PrintArea/PrintArea/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace PrintArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet sheet = workbook.Worksheets[0];
+
+                for (int i = 1; i <= 50; i++)
+                {
+                    for (int j = 1; j <= 50; j++)
+                    {
+                        sheet.Range[i, j].Text = sheet.Range[i, j].AddressLocal;
+                    }
+                }
+
+                #region PageSetup Settings
+                //Sets the range to be printed
+                sheet.PageSetup.PrintArea = "A1:M20";
+
+                #endregion
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream("PrintArea.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo("PrintArea.xlsx")
+                {
+                    UseShellExecute = true
+                };
+                process.Start();
+            }
+        }
+    }
+}
