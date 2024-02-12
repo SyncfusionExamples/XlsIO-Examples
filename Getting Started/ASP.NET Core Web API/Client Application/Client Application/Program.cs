6dcc2124a53118d3df23e083556373c38f3135bc
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2024-02-12 17:52:07 +0530

    Create Excel in ASP.NET Core Web API

---
+++ Getting Started/ASP.NET Core Web API/Client Application/Client Application/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Create an HttpClient instance
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                // Send a GET request to a URL
+                HttpResponseMessage response = await client.GetAsync("https://localhost:7000/api/Values/api/Excel");
+
+                // Check if the response is successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read the content as a string
+                    Stream responseBody = await response.Content.ReadAsStreamAsync();
+                    FileStream fileStream = File.Create("Output.xlsx");
+                    responseBody.CopyTo(fileStream);
+                    fileStream.Close();
+                }
+                else
+                {
+                    Console.WriteLine($"HTTP error status code: {response.StatusCode}");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request exception: {e.Message}");
+            }
+        }
+    }
+}
