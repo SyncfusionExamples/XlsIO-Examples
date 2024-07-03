4541e15ce57503f51f52a8dd34470485b3d40fa4
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2024-07-03 17:13:36 +0530

    Use Cases

---
+++ Use Cases/Read Excel using SFUploader/NET Standard/Read Excel using SFUploader/Read Excel using SFUploader/FileUtils.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+
+namespace Read_Excel_using_SFUploader
+{
+    public static class FileUtils
+    {
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+            => js.InvokeAsync<object>(
+               "saveAsFile",
+               filename,
+               Convert.ToBase64String(data));
+    }
+}
