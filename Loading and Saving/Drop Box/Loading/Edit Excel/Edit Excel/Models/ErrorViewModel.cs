34aed634314861a1dc83407abccdcfa86b192a17
KurmithaSF4004 <kurmitha.m@syncfusion.com>
2024-08-05 10:09:12 +0530

    Loading and Saving

--- Loading and Saving/Drop Box/Loading/Edit Excel/Edit Excel/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Edit_Excel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
