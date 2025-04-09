c9950b32fff0c3eb18c17b441cbd0de9a3ea6ce9
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-04-09 17:11:06 +0530

    951831-DeleteHyperlinksXlsioExample

---
+++ FAQ/Hyperlinks/.NET/Delete Hyperlinks/Delete Hyperlinks/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Syncfusion.XlsIO;
+using Syncfusion.XlsIO.Implementation;
+using Syncfusion.XlsIO.Implementation.Collections;
+
+namespace Delete_Hyperlinks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("Data/InputTemplate.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                // Remove first hyperlink without affecting cell styles
+                HyperLinksCollection hyperlink = worksheet.HyperLinks as HyperLinksCollection;
+                hyperlink.Remove(hyperlink[0] as HyperLinkImpl);
+
+                //Saving the workbook as stream
+                FileStream outputStream = new FileStream("Output/Output.xlsx", FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                workbook.Close();
+                excelEngine.Dispose();
+            }
+        }
+
+    }
+}
+
