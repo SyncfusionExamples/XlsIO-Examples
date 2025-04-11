2bba3e9c0dfbfb9c1882c81965959e2b16af758b
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-04-11 17:12:29 +0530

    944873-ParseWorksheetsOnDemandExample

---
+++ FAQ/Worksheet/.NET/Parse Worksheets On Demand/Parse Worksheets On Demand/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Syncfusion.XlsIO;
+
+namespace ParseWorksheetsOnDemand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream("Data/Input.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream, ExcelOpenType.Automatic, ExcelParseOptions.ParseWorksheetsOnDemand);
+
+                // Access the first worksheet (triggers parsing)
+                IWorksheet worksheet = workbook.Worksheets[0];
+                // Process your data
+                string value = worksheet.Range["A1"].Text;
+
+                // Save to file system
+                FileStream stream = new FileStream("Output/Output.xlsx", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                workbook.SaveAs(stream);
+                workbook.Close();
+                excelEngine.Dispose();
+            }
+
+        }
+
+    }
+}
