d0e978d5764c9c0cad4e5d3e8f14719c608aa620
Mohan Chandran <mohan.chandran@syncfusion.com>
2025-04-14 09:03:26 +0530

    930233-XMLtoExcel

---
+++ XML to Excel/XmlMapToExcel/.NET/XmlMapToExcel/XmlMapToExcel/Program.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.XlsIO;
+
+namespace XmlMapToExcel
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Import Xml data into the worksheet
+                FileStream inputStream = new FileStream("../../../Data/XmlFile.xml", FileMode.Open, FileAccess.Read);
+
+                //Import XML mapping to Excel
+                workbook.XmlMaps.Add(inputStream);
+
+                //Saving the workbook as stream
+                FileStream outputStream = new FileStream("../../../Output/XmlMapOutput.xlsx", FileMode.Create, FileAccess.ReadWrite);
+                workbook.SaveAs(outputStream);
+
+                //Dispose stream
+                inputStream.Dispose();
+                outputStream.Dispose();
+            }
+        }
+    }
+}
+
