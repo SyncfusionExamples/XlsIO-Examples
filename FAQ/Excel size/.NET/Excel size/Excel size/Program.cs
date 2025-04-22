d6d09fa409036caebe431097ab181a5436832a3d
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-04-22 17:51:15 +0530

    941162-ExcelSize

---
+++ FAQ/Excel size/.NET/Excel size/Excel size/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Syncfusion.XlsIO;
+
+namespace ExcelSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                worksheet.Range["A1"].Text = "Sample Data";
+
+                //Save to memory stream
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+
+                    //Compute file size in bytes
+                    long sizeInBytes = stream.Length;
+                    Console.WriteLine($"File size: {sizeInBytes} bytes");
+
+                    //Convert to KB 
+                    double sizeInKB = sizeInBytes / 1024.0;
+                    Console.WriteLine($"File size: {sizeInKB:F2} KB");
+                }
+            }
+        }
+
+    }
+}
