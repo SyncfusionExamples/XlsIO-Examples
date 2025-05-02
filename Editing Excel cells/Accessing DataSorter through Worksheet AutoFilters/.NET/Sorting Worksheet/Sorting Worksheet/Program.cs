7aa3995b66ddcac74197eff56f56f44e3d34f7c8
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-05-02 12:24:23 +0530

    941428-SortingAutofilterExample

--- Editing Excel cells/Accessing DataSorter through Worksheet AutoFilters/.NET/Sorting Worksheet/Sorting Worksheet/Program.cs
+++ Editing Excel cells/Accessing DataSorter through Worksheet AutoFilters/.NET/Sorting Worksheet/Sorting Worksheet/Program.cs
@@ -12,7 +12,7 @@ namespace Sorting_Worksheet
             {
                 IApplication application = excelEngine.Excel;
                 application.DefaultVersion = ExcelVersion.Xlsx;
-                FileStream inputStream = new FileStream(Path.GetFullPath("Data/Input.xlsx"), FileMode.Open, FileAccess.Read);
+                FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/Input.xlsx"), FileMode.Open, FileAccess.Read);
                 IWorkbook workbook = application.Workbooks.Open(inputStream);
                 IWorksheet worksheet = workbook.Worksheets[0];
 
