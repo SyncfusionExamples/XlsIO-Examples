c7027f6d86836f4bfba3e8e15541e6ab40c1cc65
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-05-09 13:11:24 +0530

    941694-TimeperiodFaqExample

---
+++ FAQ/Conditional Formatting/.NET/Time period/Time period/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Time_Period
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/Input.xlsx"), FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Apply conditional format for specific time period
+                IConditionalFormats conditionalFormats = worksheet.UsedRange.ConditionalFormats;
+                IConditionalFormat conditionalFormat = conditionalFormats.AddCondition();
+
+                //Set the format type to 'TimePeriod' to apply time-based conditional formatting
+                conditionalFormat.FormatType = ExcelCFType.TimePeriod;
+                conditionalFormat.TimePeriodType = CFTimePeriods.Today;
+
+                //Set the background color of the matching cells 
+                conditionalFormat.BackColor = ExcelKnownColors.Sky_blue;
+
+                //Saving the workbook
+                FileStream outputStream = new FileStream(Path.GetFullPath("Output/Output.xlsx"), FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+            }
+        }
+
+    }
+}
