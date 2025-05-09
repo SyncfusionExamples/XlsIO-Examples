083cd3b54bb3e5d0c501293d5f412822ade7a58d
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-05-09 17:28:50 +0530

    261772-GetWorksheetNames

---
+++ FAQ/Worksheet Names/.NET/Hidden Worksheet Names/Hidden Worksheet Names/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Syncfusion.XlsIO;
+using Syncfusion.XlsIO.Implementation.Collections;
+
+namespace Hidden_Worksheet_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+
+                FileStream inputStream = new FileStream("Data/Input.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+
+                //Get the worksheets collection
+                WorksheetsCollection worksheets = workbook.Worksheets as WorksheetsCollection;
+
+                //Print hidden worksheet names
+                foreach (IWorksheet worksheet in worksheets)
+                {
+                    if (worksheet.Visibility == WorksheetVisibility.Hidden)
+                        Console.WriteLine(worksheet.Name);
+                }
+
+                //Dispose streams
+                inputStream.Dispose();
+
+            }
+        }
+    }
+}
