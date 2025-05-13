83ab0db1c07f9c23d4820f91ed51d08b21d4cfe4
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-05-13 12:02:17 +0530

    261762-AddOvalShapeExample

---
+++ FAQ/Add oval shape to chart/.NET/Add oval shape to chart/Add oval shape to chart/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Syncfusion.Drawing;
+using Syncfusion.XlsIO;
+
+namespace Add_Oval_Shape_Chart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Add chart to worksheet
+                IChart chart = worksheet.Charts.Add();
+
+                //Add oval shape to chart
+                IShape shape = chart.Shapes.AddAutoShapes(AutoShapeType.Oval, 20, 60, 500, 400);
+
+                //Format the shape
+                shape.Line.ForeColorIndex = ExcelKnownColors.Red;
+
+                //Add the text to the oval shape and set the text alignment on the shape
+                shape.TextFrame.TextRange.Text = "This is an oval shape";
+                shape.TextFrame.VerticalAlignment = ExcelVerticalAlignment.MiddleCentered;
+                shape.TextFrame.HorizontalAlignment = ExcelHorizontalAlignment.CenterMiddle;
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream(Path.GetFullPath("Output.xlsx"), FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+            }
+        }
+    }
+}
