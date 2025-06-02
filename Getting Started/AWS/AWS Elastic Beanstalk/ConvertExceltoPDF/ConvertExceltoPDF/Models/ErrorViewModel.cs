6f92ead01d51f0b32ab9849ed451e4c10e470f8c
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-06-02 18:15:54 +0530

    855095-ExceltoPDFElasticBeanstalk

---
+++ Getting Started/AWS/AWS Elastic Beanstalk/ConvertExceltoPDF/ConvertExceltoPDF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConvertExceltoPDF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
