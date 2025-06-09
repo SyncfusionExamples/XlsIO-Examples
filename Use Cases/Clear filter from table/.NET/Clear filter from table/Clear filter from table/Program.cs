4c32d179e065e17f8b157d426e733f701a8eda4d
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-06-09 12:42:53 +0530

    264585-ClearFiltersTable

---
+++ Use Cases/Clear filter from table/.NET/Clear filter from table/Clear filter from table/Program.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.XlsIO;
+
+class Program
+{
+    public static void Main()
+    {
+        using (ExcelEngine excelEngine = new ExcelEngine())
+        {
+            IApplication application = excelEngine.Excel;
+            application.DefaultVersion = ExcelVersion.Xlsx;
+
+            FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/InputTemplate.xlsx"), FileMode.Open, FileAccess.Read);
+            IWorkbook workbook = application.Workbooks.Open(inputStream);
+            IWorksheet worksheet = workbook.Worksheets[0];
+
+            //Access the first table (ListObject) in the worksheet
+            IListObject table = worksheet.ListObjects[0];
+
+
+            //Clear filters from the table
+            table.ShowAutoFilter = false;
+
+            //Saving the workbook 
+            FileStream outputStream = new FileStream(Path.GetFullPath("Output.xlsx"), FileMode.Create, FileAccess.ReadWrite);
+            workbook.SaveAs(outputStream);
+
+            //Dispose streams
+            outputStream.Dispose();
+            inputStream.Dispose();
+        }
+    }
+}
