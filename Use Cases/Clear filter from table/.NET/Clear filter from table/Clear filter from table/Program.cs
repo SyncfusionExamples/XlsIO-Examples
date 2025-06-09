cc47c2f1009fe0a89ad24dada5fe4793a459282a
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-06-09 12:47:37 +0530

    264585-ClearFiltersTable

--- Use Cases/Clear filter from table/.NET/Clear filter from table/Clear filter from table/Program.cs
+++ Use Cases/Clear filter from table/.NET/Clear filter from table/Clear filter from table/Program.cs
@@ -1,32 +1,34 @@
 ﻿using Syncfusion.XlsIO;
 
-class Program
+namespace Clear_Filter_From_Table
 {
-    public static void Main()
+    class Program
     {
-        using (ExcelEngine excelEngine = new ExcelEngine())
+        public static void Main(string[] args)
         {
-            IApplication application = excelEngine.Excel;
-            application.DefaultVersion = ExcelVersion.Xlsx;
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
 
-            FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/InputTemplate.xlsx"), FileMode.Open, FileAccess.Read);
-            IWorkbook workbook = application.Workbooks.Open(inputStream);
-            IWorksheet worksheet = workbook.Worksheets[0];
+                FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/InputTemplate.xlsx"), FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
 
-            //Access the first table (ListObject) in the worksheet
-            IListObject table = worksheet.ListObjects[0];
+                //Access the first table (ListObject) in the worksheet
+                IListObject table = worksheet.ListObjects[0];
 
+                //Clear filters from the table
+                table.ShowAutoFilter = false;
 
-            //Clear filters from the table
-            table.ShowAutoFilter = false;
+                //Saving the workbook 
+                FileStream outputStream = new FileStream(Path.GetFullPath("Output.xlsx"), FileMode.Create, FileAccess.ReadWrite);
+                workbook.SaveAs(outputStream);
 
-            //Saving the workbook 
-            FileStream outputStream = new FileStream(Path.GetFullPath("Output.xlsx"), FileMode.Create, FileAccess.ReadWrite);
-            workbook.SaveAs(outputStream);
-
-            //Dispose streams
-            outputStream.Dispose();
-            inputStream.Dispose();
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+            }
         }
     }
 }
