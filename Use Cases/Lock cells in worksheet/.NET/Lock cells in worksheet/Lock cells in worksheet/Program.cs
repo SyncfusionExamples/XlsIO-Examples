7ff46430caf5155248cc3af8a53312fd77343646
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-06-09 14:17:36 +0530

    264700-LockOrProtectCells

---
+++ Use Cases/Lock cells in worksheet/.NET/Lock cells in worksheet/Lock cells in worksheet/Program.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.XlsIO;
+
+namespace Lock_Cells_In_Worksheet
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/InputData.xlsx"), FileMode.Open, FileAccess.ReadWrite);
+                IWorkbook workbook = application.Workbooks.Open(inputStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Unlock cells in the worksheet
+                worksheet.UsedRange.CellStyle.Locked = false; 
+
+                //Lock specific cells (A1:A5)
+                worksheet.Range["A1:A5"].CellStyle.Locked = true;
+
+                //Protect worksheet to apply lock
+                worksheet.Protect("password", ExcelSheetProtection.All);
+
+                #region Save
+                //Saving the workbook
+                FileStream outputStream = new FileStream(Path.GetFullPath("LockedCells.xlsx"), FileMode.Create, FileAccess.Write);
+                workbook.SaveAs(outputStream);
+                #endregion
+
+                //Dispose streams
+                outputStream.Dispose();
+                inputStream.Dispose();
+            }
+        }
+    }
+}
