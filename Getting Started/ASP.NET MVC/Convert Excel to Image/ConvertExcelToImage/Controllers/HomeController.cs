9a8241777a6e7a943ce814f7b4d8a8c93f8c020f
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-06-25 15:21:11 +0530

    964302-ExcelToImageNetMVCExample

---
+++ Getting Started/ASP.NET MVC/Convert Excel to Image/ConvertExcelToImage/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Syncfusion.XlsIO;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConvertExcelToImage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void ConvertExcelToImage()
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream excelStream = new FileStream("Sample.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(excelStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Convert the Excel to Image
+                Image image = worksheet.ConvertToImage(1, 1, 20, 4);
+
+                //Save the image as jpeg. 
+                ExportAsImage(image, "ExcelToImage.Jpeg", ImageFormat.Jpeg, HttpContext.ApplicationInstance.Response);
+            }
+        }
+
+        protected void ExportAsImage(Image image, string fileName, ImageFormat imageFormat, HttpResponse response)
+        {
+            if (ControllerContext == null)
+                throw new ArgumentNullException("Context");
+            string disposition = "content-disposition";
+            response.AddHeader(disposition, "attachment; filename=" + fileName);
+            if (imageFormat != ImageFormat.Emf)
+                image.Save(Response.OutputStream, imageFormat);
+            Response.End();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
