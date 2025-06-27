0ced02fe43532f9e5e122621d6354461dded63f8
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-06-27 11:08:35 +0530

    966385-ExcelToImageLinuxExample

--- Getting Started/Linux/Convert Excel to Image/ConvertExcelToImage/Program.cs
+++ Getting Started/Linux/Convert Excel to Image/ConvertExcelToImage/Program.cs
@@ -15,18 +15,17 @@ namespace ConvertExcelToImage
                 IWorkbook workbook = application.Workbooks.Open(excelStream);
                 IWorksheet worksheet = workbook.Worksheets[0];
 
-                //Initialize XlsIO renderer.
+                //Initialize XlsIORenderer
                 application.XlsIORenderer = new XlsIORenderer();
 
-                //Create the MemoryStream to save the image.      
+                //Create the MemoryStream to save the image  
                 MemoryStream imageStream = new MemoryStream();
 
-                //Save the converted image to MemoryStream.
+                //Save the converted image to MemoryStream
                 worksheet.ConvertToImage(worksheet.UsedRange, imageStream);
                 imageStream.Position = 0;
 
                 #region Save
-                //Saving the workbook
                 FileStream outputStream = new FileStream(Path.GetFullPath("Output/Sample.jpeg"), FileMode.Create, FileAccess.Write);
                 imageStream.CopyTo(outputStream);
                 #endregion
