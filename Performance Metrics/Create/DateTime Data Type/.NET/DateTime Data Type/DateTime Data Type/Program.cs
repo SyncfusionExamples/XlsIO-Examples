ffe836280a75012cca3368b8045a012941db68e0
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-08-10 18:47:22 +0530

    965994-PerformanceMetricsExamples

---
+++ Performance Metrics/Create/DateTime Data Type/.NET/DateTime Data Type/DateTime Data Type/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace DateTime_Data_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet sheet = workbook.Worksheets[0];
+
+                //Fill 150 rows × 10,000 columns with date
+                for (int row = 1; row <= 150; row++)
+                {
+                    for (int col = 1; col <= 10000; col++)
+                    {
+                        sheet[row, col].DateTime = new DateTime(2025, 1, 1).AddDays(col);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ Performance Metrics/Open/Formula Data Type/.NET/Formula Data Type/Formula Data Type/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace Formula_Data_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream fileStream = new FileStream(Path.GetFullPath(@"../../../Data/Input.xlsx"), FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(fileStream);
+
+                //Dispose streams
+                fileStream.Dispose();
+            }
+        }
+    }
+}
+
+
+
+
+
