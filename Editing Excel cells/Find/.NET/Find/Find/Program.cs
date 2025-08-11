6bae3c2c860b15018f55fcd985c411c0f3b795df
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-08-11 16:34:23 +0530

    974936-FindUGChangesExample

--- Editing Excel cells/Find/.NET/Find/Find/Program.cs
+++ Editing Excel cells/Find/.NET/Find/Find/Program.cs
@@ -16,25 +16,58 @@ namespace Find
                 IWorksheet worksheet = workbook.Worksheets[0];
 
                 //Searches for the given string within the text of worksheet
-                IRange[] result1 = worksheet.FindAll("Gill", ExcelFindType.Text);
+                IRange[] textCells = worksheet.FindAll("Gill", ExcelFindType.Text);
 
                 //Searches for the given string within the text of worksheet
-                IRange[] result2 = worksheet.FindAll(700, ExcelFindType.Number);
+                IRange[] numberCells = worksheet.FindAll(700, ExcelFindType.Number);
 
                 //Searches for the given string in formulas
-                IRange[] result3 = worksheet.FindAll("=SUM(F10:F11)", ExcelFindType.Formula);
+                IRange[] formulaCells = worksheet.FindAll("=SUM(F10:F11)", ExcelFindType.Formula);
 
                 //Searches for the given string in calculated value, number and text
-                IRange[] result4 = worksheet.FindAll("41", ExcelFindType.Values);
-
-                //Searches for the given string in comments
-                IRange[] result5 = worksheet.FindAll("Desk", ExcelFindType.Comments);
+                IRange[] valueCells = worksheet.FindAll("41", ExcelFindType.Values);
 
                 //Searches for the given string within the text of worksheet and case matched
-                IRange[] result6 = worksheet.FindAll("Pen Set", ExcelFindType.Text, ExcelFindOptions.MatchCase);
+                IRange[] textMatchingCaseCells = worksheet.FindAll("Pen Set", ExcelFindType.Text, ExcelFindOptions.MatchCase);
 
                 //Searches for the given string within the text of worksheet and the entire cell content matching to search text
-                IRange[] result7 = worksheet.FindAll("5", ExcelFindType.Text, ExcelFindOptions.MatchEntireCellContent);
+                IRange[] textMatchingEntireContentCells = worksheet.FindAll("5", ExcelFindType.Text, ExcelFindOptions.MatchEntireCellContent);
+
+                foreach (IRange cell in textCells)
+                {
+                    //Highlight found text cells in red
+                    cell.CellStyle.Color = Syncfusion.Drawing.Color.FromArgb(255, 255, 0, 0); 
+                }
+
+                foreach (IRange cell in numberCells)
+                {
+                    //Highlight found number cells in green
+                    cell.CellStyle.Color = Syncfusion.Drawing.Color.FromArgb(255, 0, 255, 0); 
+                }
+
+                foreach (IRange cell in formulaCells)
+                {
+                    //Highlight found formula cells in blue
+                    cell.CellStyle.Color = Syncfusion.Drawing.Color.FromArgb(255, 0, 0, 255); 
+                }
+
+                foreach (IRange cell in valueCells)
+                {
+                    //Highlight found value cells in orange
+                    cell.CellStyle.Color = Syncfusion.Drawing.Color.FromArgb(255, 255, 165, 0); 
+                }
+
+                foreach (IRange cell in textMatchingCaseCells)
+                {
+                    //Highlight found case-matching text cells in purple
+                    cell.CellStyle.Color = Syncfusion.Drawing.Color.FromArgb(255, 128, 0, 128); 
+                }
+
+                foreach (IRange cell in textMatchingEntireContentCells)
+                {
+                    //Highlight found entire content matching text cells in teal
+                    cell.CellStyle.Color = Syncfusion.Drawing.Color.FromArgb(255, 0, 128, 128); 
+                }
 
                 //Saving the workbook as stream
                 FileStream stream = new FileStream(Path.GetFullPath(@"Output/Find.xlsx"), FileMode.Create, FileAccess.ReadWrite);
