de32c5f7ea58576590c85b171be6de75595defda
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-08-21 21:03:02 +0530

    977456-ConditionalFormattingExamples

--- Conditional Formatting/Color Scales/.NET/Color Scales/Color Scales/Program.cs
+++ Conditional Formatting/Color Scales/.NET/Color Scales/Color Scales/Program.cs
@@ -26,7 +26,6 @@ namespace Color_Scales
                 colorScale.SetConditionCount(3);
                 colorScale.Criteria[0].FormatColorRGB = Color.FromArgb(230, 197, 218);
                 colorScale.Criteria[0].Type = ConditionValueType.LowestValue;
-                colorScale.Criteria[0].Value = "0";
 
                 colorScale.Criteria[1].FormatColorRGB = Color.FromArgb(244, 210, 178);
                 colorScale.Criteria[1].Type = ConditionValueType.Percentile;
@@ -34,10 +33,6 @@ namespace Color_Scales
 
                 colorScale.Criteria[2].FormatColorRGB = Color.FromArgb(245, 247, 171);
                 colorScale.Criteria[2].Type = ConditionValueType.HighestValue;
-                colorScale.Criteria[2].Value = "0";
-
-                conditionalFormat.FirstFormulaR1C1 = "=R[1]C[0]";
-                conditionalFormat.SecondFormulaR1C1 = "=R[1]C[1]";
 
                 #region Save
                 //Saving the workbook
