6182ff23646d8a00c14eecf07cf4766b5620df89
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-09-11 14:31:22 +0530

    981062-ModifyCharttoImageConversion

--- Chart to Image/Chart to Image/.NET/Chart to Image/Chart to Image/Program.cs
+++ Chart to Image/Chart to Image/.NET/Chart to Image/Chart to Image/Program.cs
@@ -8,25 +8,34 @@ namespace Chart_to_Image
     {
         static void Main(string[] args)
         {
+            //Initialize ExcelEngine
             using (ExcelEngine excelEngine = new ExcelEngine())
             {
+                //Initialize application
                 IApplication application = excelEngine.Excel;
+
+                //Set the default version as Xlsx
                 application.DefaultVersion = ExcelVersion.Xlsx;
 
-                // Initialize XlsIORenderer
+                //Initialize XlsIORenderer
                 application.XlsIORenderer = new XlsIORenderer();
 
-                //Set converter chart image format to PNG
+                //Set converter chart image format to PNG or JPEG
                 application.XlsIORenderer.ChartRenderingOptions.ImageFormat = ExportImageFormat.Png;
 
+                //Set the chart image quality to best
+                application.XlsIORenderer.ChartRenderingOptions.ScalingMode = ScalingMode.Best;
+
+                //Open existing workbook with chart
                 FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/InputTemplate.xlsx"), FileMode.Open, FileAccess.Read);
                 IWorkbook workbook = application.Workbooks.Open(inputStream);
                 IWorksheet worksheet = workbook.Worksheets[0];
 
+                //Access the chart from the worksheet
                 IChart chart = worksheet.Charts[0];
 
                 #region Save
-                //Saving the workbook
+                //Exporting the chart as image
                 FileStream outputStream = new FileStream(Path.GetFullPath("Output/Image.png"), FileMode.Create, FileAccess.Write);
                 chart.SaveAsImage(outputStream);
                 #endregion
