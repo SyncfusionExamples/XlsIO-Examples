a62a2ca6ce85c928c8e561cf7b10ee008e93057d
Deepika <deepika.ravi@syncfusion.com>
2025-11-04 09:45:58 +0530

    Addressed the sample for YouTube video .

---
+++ Videos/Excel_Formulas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Excel_Formulas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
