4f04b057b31e9f8bfa72eea56b19d8179008b08b
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-11-25 18:02:32 +0530

    994917-ExcelToPdfPerformanceSamples

---
+++ Performance Metrics/Excel to PDF/String Data Type/.NET/String Data Type/String Data Type/Program.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.XlsIO;
+using Syncfusion.XlsIORenderer;
+using Syncfusion.Pdf;
+using System.IO;
+
+namespace String_Data_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+
+                //Load existing Excel file
+                IWorkbook workbook = application.Workbooks.Open(Path.GetFullPath(@"Data/InputTemplate.xlsx"));
+
+                //Convert to PDF
+                XlsIORenderer renderer = new XlsIORenderer();
+                PdfDocument pdfDocument = renderer.ConvertToPDF(workbook);
+
+                #region Save
+                //Saving the workbook
+                pdfDocument.Save(Path.GetFullPath(@"Output/Sample.pdf"));
+                #endregion
+            }
+        }
+    }
+}
