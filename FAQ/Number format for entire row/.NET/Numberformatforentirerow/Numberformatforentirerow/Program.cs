8010ed9b7ded2d255858772d2868900310712a4c
Ramya Sivakumar <ramya.s@syncfusion.com>
2025-12-03 20:42:30 +0530

    992294-Numberformat

---
+++ FAQ/Number format for entire row/.NET/Numberformatforentirerow/Numberformatforentirerow/Program.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.XlsIO;
+
+class Program
+{        
+    static void Main(string[] args)
+    {
+        using (ExcelEngine excelEngine = new ExcelEngine())
+        {
+            IApplication application = excelEngine.Excel;
+            application.DefaultVersion = ExcelVersion.Xlsx;
+            IWorkbook workbook = application.Workbooks.Create(1);
+            IWorksheet worksheet = workbook.Worksheets[0];
+            
+            worksheet["A1"].Number = 1000.500;
+            worksheet["B1"].Number = 1234;
+            worksheet["C1"].Number = 54321.500;
+            worksheet["D1"].Number = .500;
+
+            worksheet["A1"].EntireRow.NumberFormat = "#,##0.0000";
+            workbook.SaveAs("../../../Output/NumberFormats.xlsx");
+        }
+    }
+}
