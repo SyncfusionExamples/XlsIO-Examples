5202885a29ec88c1a93e2aa23f0190931ace1edb
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-12-04 19:00:25 +0530

    989214-EnableFileNameWithExtensionSample

---
+++ Excel to PDF/Show file name with extension in PDF/.NET/ShowFileNameWithExtension/ShowFileNameWithExtension/Program.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.XlsIO;
+using Syncfusion.Pdf;
+using Syncfusion.XlsIORenderer;
+
+namespace ShowFileNameWithExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Open(Path.GetFullPath(@"Data/InputTemplate.xlsx"));
+
+                //Initialize XlsIORendererSettings
+                XlsIORendererSettings settings = new XlsIORendererSettings();
+
+                //Enable ShowFileNameWithExtension property
+                settings.ShowFileNameWithExtension = true;
+
+                //Initialize XlsIORenderer
+                XlsIORenderer renderer = new XlsIORenderer();
+
+                //Convert the Excel document to PDF with renderer settings
+                PdfDocument pdfDocument = renderer.ConvertToPDF(workbook, settings);
+
+                #region Save
+                //Saving the workbook
+                pdfDocument.Save(Path.GetFullPath(@"Output/Output.pdf"));
+                #endregion
+            }
+        }
+    }
+}
