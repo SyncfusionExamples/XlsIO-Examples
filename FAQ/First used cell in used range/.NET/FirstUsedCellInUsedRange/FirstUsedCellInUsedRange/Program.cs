1693093b5e7eb09b58aa3313ab4eaf58cc67c6e9
KarthikaSF4773 <karthika.santhanakrishnan@syncfusion.com>
2025-12-16 17:06:39 +0530

    991263-FirstUsedCellSample

---
+++ FAQ/First used cell in used range/.NET/FirstUsedCellInUsedRange/FirstUsedCellInUsedRange/Program.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.XlsIO;
+
+namespace FirstUsedCellInUsedRange
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Open(Path.GetFullPath(@"Data/Input.xlsx"));
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Get the used range of the worksheet
+                IRange usedRange = worksheet.UsedRange;
+
+                //Get the first cell from the used range
+                IRange firstCell = worksheet.Range[usedRange.Row, usedRange.Column];
+
+                //Get the address of the first cell
+                string firstCellAddress = firstCell.AddressLocal;
+
+                //Display the address of the first cell
+                Console.WriteLine("The address of the first used cell in used range is: " + firstCellAddress);
+
+                //Saving the workbook
+                workbook.SaveAs(Path.GetFullPath(@"Output/Output.xlsx"));
+            }
+        }
+    }
+}
