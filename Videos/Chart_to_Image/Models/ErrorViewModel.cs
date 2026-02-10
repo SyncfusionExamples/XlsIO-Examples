f2f54c3d9084ece10ccd7ecdf033c5f63280ef3d
Deepika <deepika.ravi@syncfusion.com>
2026-02-10 10:20:24 +0530

    Added the sample for image conversion video

---
+++ Videos/Chart_to_Image/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chart_to_Image.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
