37a4589db5e3167fb08e17bd53f3bd5120890abb
GowthamPonrajSF5414 <gowtham.ponraj@syncfusion.com>
2026-02-13 02:40:59 +0530

    992569-add CorruptedImg

---
+++ FAQ/Remove corrupted images/.NET/RemoveCorruptedImages/RemoveCorruptedImages/Program.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.XlsIO;
+
+namespace RemoveCorruptedImages
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Open(Path.GetFullPath(@"Data/Input.xlsx"));
+                foreach (IWorksheet sheet in workbook.Worksheets)
+                {
+                    for (int i = 0; i < sheet.Pictures.Count; i++)
+                    {
+                        if (sheet.Pictures[i].Picture.ImageData.Length <= 0)
+                        {
+                            // Remove the corrupted image.
+                            Console.WriteLine("Image removed due to corruption");
+                            sheet.Pictures[i].Remove();
+                        }
+                    }
+                }
+
+                //Save the workbook
+                workbook.SaveAs(Path.GetFullPath(@"Output/Output.xlsx"));
+            }
+        }
+    }
+}
