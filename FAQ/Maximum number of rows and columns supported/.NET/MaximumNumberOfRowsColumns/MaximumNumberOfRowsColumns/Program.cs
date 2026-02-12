9086b9e475f942eb85f5d97471dd84cb30a1e8bd
GowthamPonrajSF5414 <gowtham.ponraj@syncfusion.com>
2026-02-13 01:21:56 +0530

    999741-add MaxRowColumn example

---
+++ FAQ/Maximum number of rows and columns supported/.NET/MaximumNumberOfRowsColumns/MaximumNumberOfRowsColumns/Program.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.XlsIO;
+
+namespace MaximumNumberOfRowsColumns
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Open(Path.GetFullPath(@"Data/Input.xlsx"));
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //For getting maximum supported rows and columns  
+                int maxRow = workbook.MaxRowCount;
+                int maxColumns = workbook.MaxColumnCount;
+
+                //Display maximum number of rows and columns supported
+                Console.WriteLine("Maximum number of rows supported: " + maxRow.ToString());
+                Console.WriteLine("Maximum number of columns supported: " + maxColumns.ToString());
+            }
+        }
+        }
+}
