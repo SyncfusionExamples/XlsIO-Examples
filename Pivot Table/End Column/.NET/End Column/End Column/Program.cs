1b5ed5bc4d90e8cc082b3ae0fe8639b684ebcb3f
santiprajaSF4793 <santipraja.palanisamy@syncfusion.com>
2026-02-18 14:21:47 +0530

    989073-EndColumnPivotTable

---
+++ Pivot Table/End Column/.NET/End Column/End Column/Program.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.XlsIO;
+using System;
+using System.IO;
+
+namespace End_Column
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Open(Path.GetFullPath(@"Data/InputTemplate.xlsx"));
+                IWorksheet sheet = workbook.Worksheets[1];
+                IPivotTable pivotTable = sheet.PivotTables[0];
+
+                // Ensure layout is calculated
+                pivotTable.Layout();
+
+                // Read EndLocation from the implementation type
+                IRange endRange = (pivotTable as Syncfusion.XlsIO.Implementation.PivotTables.PivotTableImpl).EndLocation;
+                int lastColumn = endRange.LastColumn;
+
+                // Use lastColumn as needed (e.g., log)
+                Console.WriteLine("PivotTable last column: " + lastColumn);
+            }
+        }
+    }
+}
+
+
+
+
+
