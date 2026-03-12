e164e7c082deffac1732f6f5eb2dba955d1259d4
santiprajaSF4793 <santipraja.palanisamy@syncfusion.com>
2026-03-12 19:59:10 +0530

    999691-FreezePivotTable

---
+++ FAQ/Freeze Pivot Table Header/.NET/FreezePivotTableHeader/FreezePivotTableHeader/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+using Syncfusion.XlsIO.Implementation.PivotTables;
+
+namespace Edit_Pivot_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                IWorkbook workbook = application.Workbooks.Open(Path.GetFullPath(@"Data/InputTemplate.xlsx"));
+
+                //Freeze row and column
+                IWorksheet freezeSheet = workbook.Worksheets[1];
+                IRange range = freezeSheet.PivotTables[0].Location;
+                freezeSheet[range.Row + 1, range.Column + 1].FreezePanes();
+
+                #region Save
+                //Saving the workbook
+                workbook.SaveAs(Path.GetFullPath("Output/Output.xlsx"));
+                #endregion
+            }
+        }
+    }
+}
+
+
+
+
+
