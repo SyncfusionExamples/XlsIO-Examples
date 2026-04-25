0ea797e5edd3b92e92c42bb680fca7240a667f20
VinothSF5015 <vinothkumar.sampath@syncfusion.com>
2026-04-25 18:11:02 +0530

    Sample added

---
+++ FAQ/Remove ListObjects/.NET/Remove ListObjects/Program.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.XlsIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (ExcelEngine excelEngine = new ExcelEngine())
+        {
+            IApplication application = excelEngine.Excel;
+            application.DefaultVersion = ExcelVersion.Xlsx;
+            IWorkbook workbook = application.Workbooks.Open(Path.GetFullPath(@"Data/InputTemplate.xlsx"));
+            IWorksheet worksheet = workbook.Worksheets[0];
+
+            // Remove all ListObjects from the sheet
+            // Iterate in reverse order to avoid index shifting issues
+            for (int i = worksheet.ListObjects.Count - 1; i >= 0; i--)
+            {
+                IListObject listObject = worksheet.ListObjects[i];
+                worksheet.ListObjects.Remove(listObject);
+            }
+
+            workbook.SaveAs(Path.GetFullPath("Output/Output.xlsx"));
+        }
+    }
+}
