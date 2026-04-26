08e163a4fb05367849b8df71b9580676c5a9c71d
VinothSF5015 <vinothkumar.sampath@syncfusion.com>
2026-04-26 15:58:16 +0530

    999887 - Sample Added

---
+++ FAQ/Merge Cell Data/.NET/Merge Area Data/Merge Area Data/Program.cs
@@ -0,0 +1,29 @@
+﻿using Syncfusion.XlsIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an instance of ExcelEngine
+        using (ExcelEngine excelEngine = new ExcelEngine())
+        {
+            // Set the default version to Excel 2016
+            excelEngine.Excel.DefaultVersion = ExcelVersion.Excel2016;
+            // Load the workbook from the specified path
+            IWorkbook workbook = excelEngine.Excel.Workbooks.Open(Path.GetFullPath(@"Data/InputTemplate.xlsx"));
+            // Get the first worksheet in the workbook
+            IWorksheet worksheet = workbook.Worksheets[0];
+
+            //Get data from merged area
+            int row = 2, col = 5;
+
+            IRange range = worksheet[row, col];
+
+            string data = range.IsMerged ? worksheet[range.MergeArea.Row, range.MergeArea.Column].Value : range.Value;
+
+            Console.WriteLine(data);
+            // Save the workbook to a new file
+            workbook.SaveAs(Path.GetFullPath(@"Output/Output.xlsx"));
+        }
+    }
+}
