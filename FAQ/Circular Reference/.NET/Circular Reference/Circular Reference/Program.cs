f2abb2f6934d1b151ffaeeb9d9f4f566ba558b35
VinothSF5015 <vinothkumar.sampath@syncfusion.com>
2026-05-12 17:06:17 +0530

    1017888 - Sample Added

---
+++ FAQ/Circular Reference/.NET/Circular Reference/Circular Reference/Program.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.XlsIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new Excel application instance
+        using (ExcelEngine excelEngine = new ExcelEngine())
+        {
+            IApplication application = excelEngine.Excel;
+            IWorkbook workbook = application.Workbooks.Open(Path.GetFullPath("Data/InputTemplate.xlsx"));
+
+            foreach (IWorksheet worksheetxls in workbook.Worksheets)
+            {
+                worksheetxls.EnableSheetCalculations();
+                worksheetxls.CalcEngine.AllowShortCircuitIFs = true;
+                worksheetxls.CalcEngine.UseFormulaValues = true;
+                // Enable circular reference handling
+                worksheetxls.CalcEngine.ThrowCircularException = true;
+                worksheetxls.CalcEngine.IterationMaxCount = 1000;
+            }
+
+            IWorksheet worksheet = workbook.Worksheets.First();
+            Console.WriteLine(worksheet["I234"].CalculatedValue);
+            Console.WriteLine(worksheet["J234"].CalculatedValue);
+
+            workbook.SaveAs(Path.GetFullPath("Output/Output.xlsx"));
+        }
+    }
+}
