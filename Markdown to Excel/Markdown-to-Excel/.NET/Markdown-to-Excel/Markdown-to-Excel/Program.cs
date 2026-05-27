0631552dedbeb33eade914424ce48bc1bbda4d64
KARTHIKA-SF4773 <karthika.santhanakrishnan@syncfusion.com>
2026-05-27 13:32:05 +0530

    1029061-MarkdownExample

---
+++ Markdown to Excel/Markdown-to-Excel/.NET/Markdown-to-Excel/Markdown-to-Excel/Program.cs
@@ -0,0 +1,20 @@
+﻿using Syncfusion.XlsIO;
+
+namespace Markdown_to_Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+
+                IWorkbook workbook = application.Workbooks.Open(@"Data/Sample.md", ExcelOpenType.Markdown);
+
+                workbook.SaveAs(Path.GetFullPath("Output/MarkdownToExcel.xlsx"));
+            }
+        }
+    }
+}
