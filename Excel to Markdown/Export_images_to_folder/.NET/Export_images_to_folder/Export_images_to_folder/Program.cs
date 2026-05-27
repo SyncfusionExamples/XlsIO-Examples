f45defe53e1a903367216c4561d0098842eb1083
KARTHIKA-SF4773 <karthika.santhanakrishnan@syncfusion.com>
2026-05-27 16:38:40 +0530

    1029061-MarkdownExample

---
+++ Excel to Markdown/Export_images_to_folder/.NET/Export_images_to_folder/Export_images_to_folder/Program.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.XlsIO;
+
+namespace Export_images_to_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+
+                IWorkbook workbook = application.Workbooks.Open(@"Data/Markdown.xlsx");
+
+                MarkdownExportOptions exportOptions = new MarkdownExportOptions();
+                exportOptions.SaveOptions.MarkdownExportImagesFolder = @"D:/Temp/Image1.png";
+
+                using (FileStream fileStream = new FileStream(@"Output/Output.md", FileMode.Create, FileAccess.Write))
+                {
+                    workbook.SaveAs(fileStream, exportOptions);
+                }
+            }
+        }
+    }
+}
