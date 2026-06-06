cd8aab0f02947739463703a2d098c6072119e6fc
VinothSF5015 <vinothkumar.sampath@syncfusion.com>
2026-06-06 14:28:03 +0530

    Sample Added

---
+++ FAQ/Chart/.NET/Chart Axis Manual Positioning/.NET/Chart Axis Manual Positioning/Chart Axis Manual Positioning/Program.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.XlsIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (ExcelEngine excelEngine = new ExcelEngine())
+        {
+            IWorkbook workbook = excelEngine.Excel.Workbooks.Open(Path.GetFullPath(@"Data/InputTemplate.xlsx"));
+            IWorksheet worksheet = workbook.Worksheets[0];
+
+            // Access the chart in the worksheet
+            IChartShape chart = worksheet.Charts[0];
+
+            chart.PrimaryValueAxis.TitleArea.Layout.ManualLayout.Top = 0.15; // Adjust the vertical position of the title
+
+
+            // Save the modified workbook
+            workbook.SaveAs(Path.GetFullPath(@"Output/Output.xlsx"));
+        }
+    }
+}
