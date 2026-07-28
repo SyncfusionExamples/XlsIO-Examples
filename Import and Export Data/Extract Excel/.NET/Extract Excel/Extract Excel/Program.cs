60e27e495e0982d4f24ec2f6f9e439575132c0b4
KARTHIKASANTHANAKRISHNAN-SF4773 <karthika.santhanakrishnan@syncfusion.com>
2026-07-28 16:01:53 +0530

    1041188-CreateXlsIOSample

---
+++ Import and Export Data/Extract Excel/.NET/Extract Excel/Extract Excel/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Syncfusion.XlsIO;
+using System.Data;
+
+using (ExcelEngine excelEngine = new ExcelEngine())
+{
+    IApplication application = excelEngine.Excel;
+    application.DefaultVersion = ExcelVersion.Xlsx;
+    IWorkbook workbook = application.Workbooks.Open(Path.GetFullPath(@"Data/InputTemplate.xlsx"));
+    IWorksheet worksheet = workbook.Worksheets[0];
+
+    //Extract worksheet data
+    DataTable sheetData = worksheet.ExportDataTable( worksheet.UsedRange, ExcelExportDataTableOptions.ColumnNames | ExcelExportDataTableOptions.ComputedFormulaValues);
+
+    //Save the data as JSON file
+    string json = JsonConvert.SerializeObject(sheetData, Formatting.Indented);
+
+    File.WriteAllText(@"Output/Output.json", json);
+}
